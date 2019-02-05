a764ea0cce4c46c1df83fdec0102b3f4991a8b12
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-02-05 15:41:28 -0600

    changing sync method

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(routes);
 
 var db = require("./models");
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
